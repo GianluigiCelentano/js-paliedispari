38353580e57f37ef6b6c59478d93173f315ee916
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-10 00:27:39 +0200

    js-paliedispari

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ if (palindrome(checkName)) {
 }
 
 
-var sum = 0
 var oddOrEven = prompt("PARI O DISPARI?")
-var utentNumber = prompt("scrivi il tuo numero da 1 a 5")
+var utentNumber = parseInt(prompt("scrivi il tuo numero da 1 a 5"))
 function randomNum() {
     var randomNum = Math.floor(Math.random() * 5)
     return randomNum
 }
-sum = parseInt(utentNumber) + randomNum
 
-if (sum % 2 === 0 && parseInt(utentNumber) % 2 === 0) {
-    document.getElementById("winner").innerHTML += "PARI" + " " + randomNum(sum)
+var randomNum = utentNumber + randomNum
+
+if (sum % 2 === 0 && sum === utentNumber) {
+    document.getElementById("winner").innerHTML += "PARI" + " " + randomNum
 } else {
-    document.getElementById("winner").innerHTML += "DISPARI"+ " " + randomNum(sum)
+    document.getElementById("winner").innerHTML += "DISPARI"+ " " + randomNum
 }
